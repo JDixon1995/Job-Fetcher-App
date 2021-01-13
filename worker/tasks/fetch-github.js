e0555c64df9a36c681db9cfa06110723f6e80994
John <jddhalogamer@gmail.com>
2021-01-12 18:15:17 -0600

    started building the back end and adding the API, and node.js

---
+++ worker/tasks/fetch-github.js
@@ -0,0 +1,10 @@
+var fetch = require('node-fetch');
+
+const baseURL = 'https://jobs.github.com/positions.json'
+
+export default async function fetchGithub() {
+    const jobs = await fetch(baseURL);
+    console.log(jobs.length);
+}
+
+fetchGithub();
